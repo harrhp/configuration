801b23460a3e36c82db7a4fb51d9540d201279f4
harrhp <>
2024-03-08 19:36:46 +0300

    switch from bash to pwsh on linux

--- install.sh
+++ install.sh
@@ -1,31 +1,87 @@
 #!/usr/bin/env bash
 
-cp ./gitconfig/.gitconfig ~/
+set -e
 
-if command -v aliae > /dev/null && command -v oh-my-posh > /dev/null ; then
-  PROFILE=~/.profile
-  sed -i "/^# dotfiles$/,/^# dotfiles$/d" $PROFILE
-  echo \
-  '# dotfiles
-export ALIAE_CONFIG="https://raw.githubusercontent.com/harrhp/configuration/HEAD/shell/aliae.yaml"
-eval "$(aliae init bash)"
-# dotfiles' >> $PROFILE
-fi
+log() {
+  GREEN='\033[0;32m'
+  NC='\033[0m'
+  printf "%b%s%b\n" "$GREEN" "$1" "$NC"
+}
 
-sudo cp ./self-signed-root-ca/root-ca.crt /usr/local/share/ca-certificates
-sudo update-ca-certificates
+ensureWinExeSymlink() {
+  mkdir -p $(dirname "$2")
+  winExePath=$(command -v $1 || true)
+  if [ -f "$winExePath" ] ; then
+    log "symlinking $winExePath to $2"
+    rm -f "$2" && ln -s "$winExePath" "$2"
+  else
+    log "did not create symlink for $1 because it does not exist"
+  fi
+}
 
-if [ -n "${WSL_DISTRO_NAME+isset}" ] ; then
-  BIN=~/bin
+is_wsl() {
+  [ -n "${WSL_DISTRO_NAME+isset}" ]
+}
+
+install_gitconfig() {
+  log "installing gitconfig"
+  cp ./gitconfig/.gitconfig ~/
+}
+
+install_pwsh_profile() {
+  log "installing pwsh profile"
+  mkdir -p ~/.config/powershell/
+  cp ./pwsh-profile/Microsoft.PowerShell_profile.ps1 $_
+}
+
+install_self_signed_ca() {
+  log "installing ca"
+  sudo cp ./self-signed-root-ca/root-ca.crt /usr/local/share/ca-certificates
+  sudo update-ca-certificates
+}
+
+install_pwsh() {
+  log installing powershell
+  sudo apt-get update
+  sudo apt-get install -y wget apt-transport-https software-properties-common
+  source /etc/os-release
+  wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+  sudo dpkg -i packages-microsoft-prod.deb
+  rm packages-microsoft-prod.deb
+  sudo apt-get update
+  sudo apt-get install -y powershell
+}
+
+install_tools() {
+  log "installing tools"
+  curl -s https://aliae.dev/install.sh | sudo bash -s
+  curl -s https://ohmyposh.dev/install.sh | sudo bash -s
+  install_pwsh
+  log "tools installed"
+}
+
+install() {
   mkdir -p $BIN
+  install_gitconfig
 
-  ensureWinExeSymlink() {
-    mkdir -p $(dirname "$2")
-    winExePath=$(command -v $1)
-    if [ -f "$winExePath" ] ; then
-      rm -f "$2" && ln -s "$winExePath" "$2"
-    fi
-  }
+  SOURCE="$1"
+  log "running with source: ${SOURCE:-default}"
+  case "$SOURCE" in
+    tools)
+      install_tools
+      ;;
+    *)
+      ;;
+  esac
 
-  ensureWinExeSymlink ssh.exe $BIN/wssh
-fi
+  install_pwsh_profile
+  install_self_signed_ca
+
+  if is_wsl ; then
+    log "running in wsl"
+    ensureWinExeSymlink ssh.exe $BIN/wssh
+  fi
+}
+
+BIN=~/bin
+install "$@"
---
+++ pwsh-profile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+$env:ALIAE_CONFIG = "https://raw.githubusercontent.com/harrhp/configuration/HEAD/shell/aliae.yaml";
+aliae init pwsh | Invoke-Expression
